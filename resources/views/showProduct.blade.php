dd9295377340184dbba728c86ce8043b93c5a2d2
olafschouten <olafschouten99@gmail.com>
2020-05-26 09:17:03 +0200

    add cart link to product page

--- resources/views/showProduct.blade.php
+++ resources/views/showProduct.blade.php
@@ -13,6 +13,11 @@
                     <h3>{{ $product->title }}</h3>
                     <div class="col-md-auto">{{ $product->description }}</div>
                     <p>${{ $product->price }}</p>
+                    <a href="{{ route('product.addToCart', ['id' => $product->id]) }}"
+                       class="btn btn-primary pull-left" role="button">Add to cart</a>
+
+                    <br>
+                    <br>
                     @foreach($categories as $category)
                         <a href="/categories/{{ $category->id }}">{{ $category->title }}</a>
                     @endforeach
